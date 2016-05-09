c3a930b0fcab8456e4db20f5d5a072793ba74902
githubcarlos <carlos.fedez.cuadrado@gmail.com>
2016-05-09 20:47:20 +0200

    Pagina creditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,3 +10,8 @@ exports.check = function(req, res, next){
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
 	res.render('quizzes/result', {result: result, answer: answer});
 };
+
+//GET /check
+exports.check = function(req, res, next){
+	res.render('quizzes/author');
+}
