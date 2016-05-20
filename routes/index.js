0023dbc67d03f149111293dc9ed7e0399ef50e9b
githubcarlos <carlos.fedez.cuadrado@gmail.com>
2016-05-20 12:07:46 +0200

    tema12: Crear preguntas - 6

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,24 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-//Autoload de rutas que usen :quizId
+
+// Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+
 // Definición de rutas de /quizzes
+router.get('/author',   quizController.author);
+
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-router.get('/quizzes/new', 				   quizController.new);
-router.post('/quizzes', 				   quizController.create);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
 
-router.get('/author',   quizController.author);
+
 
 module.exports = router;
