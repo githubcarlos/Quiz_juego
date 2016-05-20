0c728914d6c36d7d30b8d621800b82b3afab41b1
githubcarlos <carlos.fedez.cuadrado@gmail.com>
2016-05-20 11:17:19 +0200

    tema12: Crear preguntas - 3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,19 @@
 var models = require('../models');
 
-//Autoload el quiz asociado a :quizId
-exports.load = function(req, res, next, quizId){
+// Autoload el quiz asociado a :quizId
+exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId)
-		.then(function(quiz){
-			if (quiz) {
-				req.quiz = quiz;
-				next();
-			}else{
-				next(new Error ('No existe quizId = ' + quizId));
-			}
-		}).catch(function(error){
-			next(error);
-		});
+  		.then(function(quiz) {
+      		if (quiz) {
+        		req.quiz = quiz;
+        		next();
+      		} else { 
+      			throw new Error('No existe quizId=' + quizId);
+      		}
+        })
+        .catch(function(error) { next(error); });
 };
+
 
 // GET /quizzes
 exports.index = function(req, res, next) {
@@ -29,20 +29,26 @@ exports.index = function(req, res, next) {
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
+
 	var answer = req.query.answer || '';
-	res.render('quizzes/show', { quiz: req.quiz, answer: answer});
+
+	res.render('quizzes/show', {quiz: req.quiz,
+								answer: answer});
 };
 
 
 // GET /quizzes/:id/check
 exports.check = function(req, res, next) {
-	var answer = req.query.answer || '';
-	var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', { quiz: req.quiz, 
-									result: result, 
-									answer: answer });
 
+	var answer = req.query.answer || "";
+
+	var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+
+	res.render('quizzes/result', { quiz: req.quiz, 
+								   result: result, 
+								   answer: answer });
 };
+
 
 // GET /quizzes/new
 exports.new = function(req, res, next) {
