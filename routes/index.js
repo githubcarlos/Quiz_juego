499776c3a5b24c4ee4a327b578df2de5f5098fa9
githubcarlos <carlos.fedez.cuadrado@gmail.com>
2016-05-20 13:56:06 +0200

    tema15: Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,14 @@ router.param('quizId', quizController.load);  // autoload :quizId
 
 
 // Definición de rutas de /quizzes
-router.get('/author',   quizController.author);
-
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 
-
+router.get('/author',   quizController.author);
 
 module.exports = router;
