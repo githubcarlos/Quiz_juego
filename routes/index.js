5687095ce4cceff46e0de06c05e84ad589a49211
githubcarlos <carlos.fedez.cuadrado@gmail.com>
2016-05-27 14:02:51 +0200

    tema24: MVC de aceptar comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res, next) {
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 router.param('userId', userController.load);  // autoload :userId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/session',    sessionController.new);     // formulario login
@@ -42,9 +43,11 @@ router.get('/quizzes/:quizId(\\d+)/edit',     sessionController.loginRequired, q
 router.put('/quizzes/:quizId(\\d+)',          sessionController.loginRequired, quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',       sessionController.loginRequired, quizController.destroy);
 
-//Para comentarios
+//Rutas de /comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',    sessionController.loginRequired, commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', 
+	                                              sessionController.loginRequired, commentController.accept);
 
 //GET author page
 router.get('/author',   quizController.author);
